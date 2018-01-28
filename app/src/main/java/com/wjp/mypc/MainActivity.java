61a17a48876a8afe3fd662d81a3be98ada160c68
温建平 <1658159330@qq.com>
2018-01-28 15:33:14 +0800

    添加SlidingMenu类库成功

--- app/src/main/java/com/wjp/mypc/MainActivity.java
+++ app/src/main/java/com/wjp/mypc/MainActivity.java
@@ -3,14 +3,25 @@ package com.wjp.mypc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.wjp.mypc.librarydemo.Utils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Utils.testLib();
         setContentView(R.layout.activity_main);
-        Utils.testLib();
+        setBehindContentView(R.layout.slidingmenu_left_test);
+
+        SlidingMenu mneu=getSlidingMenu();
+        getSlidingMenu().setMode(SlidingMenu.LEFT);
+        mneu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        mneu.setTouchModeBehind(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        mneu.setBehindOffset(300);
     }
 }
