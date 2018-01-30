ce07551692198332984ec10d8b8cac2ffc60acf6
温建平 <1658159330@qq.com>
2018-01-30 17:59:19 +0800

    初步使用xUtils成功

--- app/src/main/java/com/wjp/mypc/base/impl/NewsCenterPager.java
+++ app/src/main/java/com/wjp/mypc/base/impl/NewsCenterPager.java
@@ -11,6 +11,7 @@ import com.wjp.mypc.base.BasePager;
 
 import org.xutils.common.Callback;
 import org.xutils.common.util.LogUtil;
+import org.xutils.http.RequestParams;
 import org.xutils.x;
 
 
@@ -34,14 +35,17 @@ public class NewsCenterPager extends BasePager {
         tvTitle.setText("新闻中心");
         //显示menu图标
         imgBtnMenu.setVisibility(View.VISIBLE);
-//        getDataFromService();
+        getDataFromService();
     }
 
     /*
     * 从服务器获取数据，获取之前需要权限
     * */
     private void   getDataFromService(){
-        BaiduParams params=new BaiduParams();
+//        BaiduParams params=new BaiduParams();
+        RequestParams params = new RequestParams("http://localhost:8080/zhbj/categories.json");
+//        params.addQueryStringParameter("username","abc");
+//        params.addQueryStringParameter("password","123");
         Callback.Cancelable cancelable= x.http().get(params,
                 new Callback.CacheCallback<String>() {
                     @Override
@@ -66,8 +70,7 @@ public class NewsCenterPager extends BasePager {
 
                     @Override
                     public void onSuccess(String result) {
-                        Toast.makeText(x.app(), "success", Toast.LENGTH_LONG).show();
-                        LogUtil.d(result.toString());
+                        System.out.println("结果:"+result);
                     }
                 });
     }
